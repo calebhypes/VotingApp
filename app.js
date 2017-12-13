7089899237617f34f8d5396ce8b60cafe1198c10
Caleb Cox <caleb@caleb-cox.com>
2017-12-13 13:56:14 -0500

    Updated error handling for undefined routes/404

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ app.use((req, res, next) => {
 app.use('/', indexRoutes);
 app.use('/polls', pollRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
 app.listen(3000, () =>  {
     console.log('Voting server is running...');
 });
